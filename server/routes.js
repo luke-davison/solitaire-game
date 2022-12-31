c7be97791f7606a417065cedd450f8638704b7b6
Luke Davison <luke.davison@bpsoftware.net>
2023-01-01 12:51:31 +1300

    Removed database

--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,11 @@
 const { getGameDeck } = require('./newGame')
-const { updateCount } = require('./db')
 
 const path = require('path')
 var express = require('express')
 var router = express.Router()
 
-// const game1Coordinates = String(process.env.game1Coordinates || '374912317521234')
-
 router.post('/submit', function (req, res) {
   const game = Number(req.body.game)
-  // if (game === 1) {
-  //   return res.send({message: 'Congratulations!.  The coordinates are: ' + game1Coordinates})
-  // }
   if (game === 2) {
     return res.send({message: 'Well done on completing the game.  There are no co-ordinates here.  Try again and pay more attention to the cards.'})
   }
@@ -23,7 +17,6 @@ router.post('/getdeck', function (req, res) {
   if (!game || game > 2 || game < 1) {
     game = 1
   }
-  updateCount(game)
   const deck = getGameDeck(game)
   return res.send({cardIds: deck})
 })
